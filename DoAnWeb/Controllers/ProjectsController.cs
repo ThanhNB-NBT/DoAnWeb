792b00d79dc56944bd502b1bf9ed57a1c9116b92
thanhpe <nguyenthanh120223@gmail.com>
2024-01-07 23:07:16 +0700

    thêm lọc dự án

--- DoAnWeb/Controllers/ProjectsController.cs
+++ DoAnWeb/Controllers/ProjectsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DoAnWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnWeb.Controllers
 {
@@ -12,10 +13,25 @@ namespace DoAnWeb.Controllers
             _logger = logger;
             _context = context;
         }
+        //public IActionResult Index()
+        //{
+        //    var projects = _context.Projects.Where(m => m.IsActive).OrderByDescending(i => i.ProjectId).ToList();
+        //    return View(projects);
+        //}
         public IActionResult Index()
         {
-            var projects = _context.Projects.Where(m => m.IsActive).OrderByDescending(i => i.ProjectId).ToList();
-            return View(projects);
+            var projects = _context.Projects
+        .Include(p => p.CategoryP)
+        .Where(m => m.IsActive)
+        .OrderByDescending(i => i.ProjectId)
+        .ToList();
+
+            var categories = _context.CategoryProjects.ToList();
+
+            ViewBag.Projects = projects;
+            ViewBag.Categories = categories;
+
+            return View();
         }
         [Route("/projects-{slug}-{id:}.html", Name = "Details")]
         public IActionResult Details(long? id)
